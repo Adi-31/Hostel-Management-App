740d115cf4396de6c1c38d0615397d74761c26cd
Anna S Antony <annasantony@Annas-MacBook-Air.local>
2022-11-07 23:50:37 +0530

    profile page added

---
+++ src/components/ProfileLogout.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProfileLogout = () => {
+  return (
+    <View style={{ marginTop:180,padding:10,alignItems:'center',justifyContent:'center',backgroundColor:'#E24C4B',borderRadius:20,marginLeft:30,marginRight:30}}>
+      <Text style={{fontSize:18,color:'white'}}>Logout</Text>
+    </View>
+  )
+}
+
+export default ProfileLogout
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,10 +1,24 @@
 import { View, Text } from "react-native";
 import React from "react";
+import ProfileHeader from "./ProfileHeader";
+import ProfileContent from "./ProfileContent";
+import ProfileContent2 from "./ProfileContent2";
+import ProfileDue from "./ProfileDue";
+import ProfileTotDue from "./ProfileTotDue";
+import ProfileWallet from "./ProfileWallet";
+import ProfileLogout from "./ProfileLogout";
+
 
 const ProfilePage = () => {
   return (
     <View>
-      <Text>Profile page comes here</Text>
+      <ProfileHeader/>
+      <ProfileContent/>
+      <ProfileContent2/>
+      <ProfileDue/>
+      <ProfileTotDue/>
+      <ProfileWallet/>
+      <ProfileLogout/>
     </View>
   );
 };
