749bc904aa9bcfc57ad3248cc1d7612ad145b38a
sabeeh k <sabeehk@sabeehs-MacBook-Air.local>
2022-11-07 22:38:30 +0530

    styles changed to stylesheet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text , StyleSheet} from "react-native";
 import { SIZES } from "../constants/theme";
 import { CircleButton } from "./Button";
 
 const Header = () => {
   return (
     <View
-      style={{
-        flexDirection: "row",
-        height: 75,
-        backgroundColor: "#3358F9",
-      }}
+      style={styles.container}
     >
       {/* left side text  */}
-      <View
-        style={{
-          flex: 1,
-          marginLeft: 10,
-          padding: 10,
-        }}
-      >
-        <Text style={{ fontSize: SIZES.medium, color: "white", opacity: 0.5 }}>
+      <View style={styles.leftsideview} >
+        <Text style={styles.leftsidetext1}>
           Good Morning
         </Text>
-        <Text
-          style={{
-            fontSize: SIZES.large * 2,
-            color: "white",
-            fontWeight: "bold",
-          }}
-        >
+        <Text style={styles.leftsidetext2}>
           Anna
         </Text>
       </View>
       {/* profile icon  */}
       <View
-        style={{
-          flex: 1,
-          alignItems: "space-between",
-          justifyContent: "center",
-          // padding: 10,
-          marginRight: 10,
-        }}
+        style={styles.profileiconview}
       >
         <CircleButton />
       </View>
@@ -49,3 +27,11 @@ const Header = () => {
 };
 
 export default Header;
+
+const styles= StyleSheet.create({
+  container : { flexDirection: "row", height: 75, backgroundColor: "#3358F9",},
+  leftsideview : { flex: 1,  marginLeft: 10, padding: 10,},
+  leftsidetext1 : { fontSize: SIZES.medium, color: "white", opacity: 0.5 },
+  leftsidetext2 : { fontSize: SIZES.large * 2,color: "white", fontWeight: "bold"},
+  profileiconview : {flex: 1, alignItems: "space-between", justifyContent: "center",marginRight: 10,}
+})
